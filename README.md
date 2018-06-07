0675578814b855c9a72843354fa2eb2959295509
Handrew <andrew.a.han@gmail.com>
2018-06-07 01:33:42 -0400

    restarting my portfolio

--- README.md
+++
@@ -1,4 +0,0 @@
-# ahan1986.github.io
-
-Hello!
-My name is Andrew Han.  I would like to welcome you to my portfolio page.  
--- routes/html-routes.js
+++
@@ -1,9 +0,0 @@
-// Dependencies
-var path = require('path');
-
-//routes
-module.exports = (app) => {
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../assets/index.html"));
-      });
-};
